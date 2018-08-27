6ba0c50c792ee447afdbfb1ecdaee0e410587726
Terry <38963056+zoegodterry@users.noreply.github.com>
2018-08-27 08:32:31 -0400

    sqlite -> mongodb

--- monitor.py
+++ monitor.py
@@ -3,9 +3,43 @@ import asyncio
 import math
 import sqlite3
 import time
+from pymongo import MongoClient
 from bs4 import BeautifulSoup as soup
 from random import randint, choice
 from discord_hooks import Webhook
+
+
+class SupremeDatabase(object):
+    """A mongo database that contains accumulated supreme products."""
+
+    def __init__(self):
+        mongoURL = "mongodb://terry:terry123@ds133632.mlab.com:33632/monitors"
+        client = MongoClient(mongoURL)
+        db = client["monitors"]
+        self.keys = db["supreme"]
+
+    # Fetches a object from the database with the matching link criteria
+    def database_fetch(self, link):
+        return self.keys.find_one({"link": link})
+
+    # Inserts a supreme product into the database and stores it for further use
+    def insert_product(self, name, link, image, sold_out, price):
+        keys = self.keys
+        user_key = self.keys.find_one({"link": link})
+        if user_key is None:
+            post = {
+                "name": name,
+                "link": link,
+                "image": image,
+                "sold_out": sold_out,
+                "price": price
+            }
+            self.keys.insert_one(post)
+        else:
+            print(f"{name} already exists in database.")
+
+    def update_product(self, _query, _set):
+        self.keys.find_one_and_update(_query, _set)
 
 
 async def create_webhooks(color=0x0061ff):
@@ -22,44 +56,30 @@ async def initialize():
 
     generalHeaders = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"}
 
-    conn = sqlite3.connect('products.db')
-    c = conn.cursor()
+    mongoSupreme = SupremeDatabase()
 
-    try:
-
-        # If the table already exist it'll throw an error
-        with conn:
-            c.execute("""CREATE TABLE products (
-                        name text,
-                        link text,
-                        image text,
-                        sold_out integer,
-                        price text
-
-                )""")
-
-        await startup("http://www.supremenewyork.com/shop/all", proxies, generalHeaders, conn, c)
-
-    except Exception as e:
-        print(e)
-
-    while True:
-        await monitor("http://www.supremenewyork.com/shop/all", proxies, generalHeaders, conn, c)
-        time.sleep(delay)
+    if mongoSupreme.keys.find_one() is None:
+        await startup("http://www.supremenewyork.com/shop/all", proxies, generalHeaders, mongoSupreme)
+    else:
+        while True:
+            await monitor("http://www.supremenewyork.com/shop/all", proxies, generalHeaders, mongoSupreme)
+            time.sleep(delay)
 
 
 # Creates an initial database
-async def startup(link, proxies, headers, conn, c):
+async def startup(link, proxies, headers, mongoSupreme):
 
     # 0 False
     # 1 True
+
+    print("Scraping supremenewyork")
 
     async with aiohttp.ClientSession() as s:
         response = await fetch(s, link, headers, choice(proxies))
         html_soup = soup(response, "html.parser")
         products = html_soup.findAll("div", {"class": "inner-article"})
 
-        async def productInformation(product, s, conn, c):
+        async def productInformation(product, s, mongoSupreme):
             link = f'https://www.supremenewyork.com{product.a["href"]}'
 
             image = f'https:{product.a.img["src"]}'
@@ -73,66 +93,59 @@ async def startup(link, proxies, headers, conn, c):
             except:
                 price = "$"
 
-            with conn:
-                c.execute("INSERT INTO products VALUES (:name, :link, :image, :sold_out, :price)", {"name": name, "link": link, "image": image, "sold_out": sold_out * 1, "price": price})
-
+            mongoSupreme.insert_product(name, link, image, sold_out, price)
             print(f"{name} added to database.")
 
-        futures = [asyncio.ensure_future(productInformation(product, s, conn, c)) for product in products]
+        futures = [asyncio.ensure_future(productInformation(product, s, mongoSupreme)) for product in products]
         await asyncio.gather(*futures)
 
-        c.execute("SELECT * FROM products")
-        all_db_items = c.fetchall()
-        print(f"Monitoring {len(all_db_items)} products on Supreme site.")
+        all_db_items = mongoSupreme.keys.count_documents({})
+        print(f"Monitoring {all_db_items} products on Supreme site.")
         webhooks = await create_webhooks(0x0061ff)  # bright blue
         futures = [asyncio.ensure_future(webhook.apost(Announcement=f"Monitoring **{len(all_db_items)} products** on [Supreme]({link}) site.")) for webhook in webhooks]
         await asyncio.gather(*futures)
 
 
 # Compares newly fetched supreme site from database
-async def monitor(link, proxies, headers, conn, c):
+async def monitor(link, proxies, headers, mongoSupreme):
     # 0 False
     # 1 True
+
+    print("Monitoring supremenewyork")
 
     async with aiohttp.ClientSession() as s:
         response = await fetch(s, link, headers, choice(proxies))
         html_soup = soup(response, "html.parser")
         products = html_soup.findAll("div", {"class": "inner-article"})
 
-        async def monitorProduct(product, s, conn, c):
+        async def monitorProduct(product, s, mongoSupreme):
             link = f'https://www.supremenewyork.com{product.a["href"]}'
             sold_out = "sold out" in product.text
-            database_product = await database_fetch(link, c)
+            database_product = mongoSupreme.database_fetch(link)
 
             if database_product is not None:
 
                 # Check if database sold_out = True -> False (Restock)
-                if database_product[3] == True and sold_out == False:
-                    print(f"{database_product[0]} restocked!")
+                if database_product["sold_out"] == True and sold_out == False:
+                    print(f"{database_product['name']} restocked!")
                     # Send restock embed
                     webhooks = await create_webhooks(0x00ff4c)  # bright green
-                    futures = [asyncio.ensure_future(webhook.apost(Restock=database_product[0], Link=database_product[1], Image=database_product[2], Price=database_product[4])) for webhook in webhooks]
+                    futures = [asyncio.ensure_future(webhook.apost(Restock=database_product["name"], Link=database_product["link"], Image=database_product["image"], Price=database_product["price"])) for webhook in webhooks]
                     await asyncio.gather(*futures)
 
                     # Update database
-                    with conn:
-                        c.execute("""UPDATE products SET sold_out = :sold_out
-                                    WHERE link = :link""",
-                                  {'link': link, 'sold_out': 0})
+                    mongoSupreme.update_product({"link": link}, {"sold_out": sold_out})
 
                 # Check if database sold_out = False -> True (Sold Out)
-                elif database_product[3] == False and sold_out == True:
-                    print(f"{database_product[0]} is now sold out.")
+                elif database_product["sold_out"] == False and sold_out == True:
+                    print(f"{database_product['name']} is now sold out.")
                     # Send sold-out embed
                     webhooks = await create_webhooks(0xc11300)  # red
-                    futures = [asyncio.ensure_future(webhook.apost(SoldOut=database_product[0], Image=database_product[2])) for webhook in webhooks]
+                    futures = [asyncio.ensure_future(webhook.apost(SoldOut=database_product["name"], Image=database_product["name"])) for webhook in webhooks]
                     await asyncio.gather(*futures)
 
                     # Update database
-                    with conn:
-                        c.execute("""UPDATE products SET sold_out = :sold_out
-                                    WHERE link = :link""",
-                                  {'link': link, 'sold_out': 1})
+                    mongoSupreme.update_product({"link": link}, {"sold_out": sold_out})
 
             else:  # Product does not exist in database
                 image = f'https:{product.a.img["src"]}'
@@ -144,16 +157,15 @@ async def monitor(link, proxies, headers, conn, c):
                 except:
                     price = "$"
 
-                with conn:
-                    c.execute("INSERT INTO products VALUES (:name, :link, :image, :sold_out, :price)", {"name": name, "link": link, "image": image, "sold_out": sold_out * 1, "price": price})
-
+                mongoSupreme.insert_product(name, link, image, sold_out, price)
                 print(f"{name} added to database.")
+
                 # Send new-product embed
                 webhooks = await create_webhooks(0xf2ff00)  # bright yellow
                 futures = [asyncio.ensure_future(webhook.apost(New=name, Link=link, Image=image, Price=price)) for webhook in webhooks]
                 await asyncio.gather(*futures)
 
-        futures = [asyncio.ensure_future(monitorProduct(product, s, conn, c)) for product in products]
+        futures = [asyncio.ensure_future(monitorProduct(product, s, mongoSupreme)) for product in products]
         await asyncio.gather(*futures)
 
 
@@ -181,12 +193,6 @@ async def fetch(session, url, headers=None, proxy=None):
     # print(f"Using proxy: {proxy}")
     async with session.get(url, headers=headers, proxy=proxy) as response:
         return await response.text()
-
-
-# Fetches a object from the database with the matching link criteria
-async def database_fetch(link, c):
-    c.execute("SELECT * FROM products WHERE link=:link", {'link': link})
-    return c.fetchone()
 
 
 if __name__ == "__main__":
